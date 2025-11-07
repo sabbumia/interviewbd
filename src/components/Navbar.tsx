ed5e24fb032b68bc0870a10333a79642fb0af4f5
sabbumia <dwmaster22@gmail.com>
2025-11-08 02:46:01 +0600

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,8 @@ import {
   ChevronDown,
   ShieldCheck,
   Check,
-  Crown
+  Crown,
+  UserStar
 } from 'lucide-react';
 import { useAuth } from '@/context/AuthContext';
 
@@ -204,7 +205,7 @@ export default function Navbar() {
                           {user.name}
                         </div>
                         <div className="text-xs text-gray-500 capitalize">
-                          <div className="flex items-center">{user.role === "admin" && <Crown className="w-3 h-3 text-red-400 mr-1" />} {user.role === "moderator" && <ShieldCheck className="w-3 h-3 text-orange-400 mr-1" />}  {user.role}</div>
+                          <div className="flex items-center">{user.role === "admin" && <Crown className="w-3 h-3 text-red-400 mr-1" />} {user.role === "moderator" && <ShieldCheck className="w-3 h-3 text-orange-400 mr-1" />} {user.role === "user" && <UserStar className="w-3 h-3 text-orange-400 mr-1" />} {user.role}</div>
                         </div>
                       </div>
                       <ChevronDown
